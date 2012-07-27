8f43932e0bdb1ab3d578bdc333d4ab4e7652674a
Steve Shogren <steve.a.shogren@gmail.com>
2012-07-27 16:24:59 -0400

    additional cleaning

--- Euler3.rb
+++ Euler3.rb
@@ -1,6 +1,5 @@
-
 require 'mathn'
-def Problem3(numberToFactor, beginning_time)
+def Problem3(numberToFactor)
   div = 2
   while true do
     if (numberToFactor % div) == 0
@@ -14,29 +13,6 @@ def Problem3(numberToFactor, beginning_time)
       end
     else
       div += 1
-    end
-  end
-end
-
-def NaiveProblem3(numberToFactor, beginning_time)
-  numberToFactor = (numberToFactor.pop / 2).ceil
-  puts numberToFactor 
-  num = numberToFactor  
-  while num > 0 do
-    if num % 2 != 0 
-      if numberToFactor % num == 0 
-       if num.prime?
-          puts "largest prime factor of #{numberToFactor} is: #{num}"
-          return num
-       end
-      end
-    end
-    num-=1
-    if num % 1000000 == 0
-      test = num.to_f / numberToFactor.to_f
-      test = test * 100
-      end_time = Time.now
-      puts "#{num}/#{numberToFactor} - #{test}% - #{(end_time - beginning_time)}"
     end
   end
 end
--- Greeting.rb
+++ Greeting.rb
@@ -33,7 +33,7 @@ end
 
 def time_method(method, *args)
   beginning_time = Time.now
-  self.send(method, args, beginning_time)
+  self.send(method, args[0])
   end_time = Time.now
   puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
 end
@@ -42,7 +42,7 @@ learningPath = '~\Ruby\learning\\'
 require learningPath + 'Euler1.rb'
 require learningPath + 'Euler2.rb'
 require learningPath + 'Euler3.rb'
+require learningPath + 'Euler4.rb'
 if __FILE__ == $0
-  Problem3(600851475143, 1)
-  #Problem3(13000, 1)
+  time_method(:Problem3, 600851475143)
 end
