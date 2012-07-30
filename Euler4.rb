a186d77dcfac9cefeed4f3b45a105c434a1868fb
Steve Shogren <steve.a.shogren@gmail.com>
2012-07-30 15:33:43 -0400

    problem 4 completed

--- Euler4.rb
+++ Euler4.rb
@@ -1,1 +1,31 @@
+require 'mathn'
+def Problem4(digits) 
+  highestFactor = (10**digits) - 1
+  highestNumber = highestFactor * highestFactor 
+  while true do
+    if isPalindrome(highestNumber) and divisibleByNumberOrLower(highestNumber, highestFactor)
+      puts "Highest palindrome of two #{digits}s numbers is : #{highestNumber}"
+      return highestNumber
+    end
+    highestNumber -= 1
+  end
+end
+
+def isPalindrome(num)
+  org_num = num.to_s()
+  return (num.to_s().reverse! == org_num)
+end
+
+def divisibleByNumberOrLower(num, divider)
+  orig_divider = divider
+  while divider > 1 do
+    if (num % divider == 0) and num / divider < orig_divider
+      puts "divisible by #{divider}"
+      return true
+    else
+      divider -= 1
+    end
+  end 
+  return false
+end
 
--- runner.rb
+++ runner.rb
@@ -1,3 +1,20 @@
+def time_method(method, *args)
+  beginning_time = Time.now
+  self.send(method, args[0])
+  end_time = Time.now
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
+end
+
+learningPath = '~\Ruby\learning\\'
+require learningPath + 'Euler1.rb'
+require learningPath + 'Euler2.rb'
+require learningPath + 'Euler3.rb'
+require learningPath + 'Euler4.rb'
+if __FILE__ == $0
+  time_method(:Problem4, 3)
+end
+
+
 class MegaGreeter
   attr_accessor :names
 
@@ -29,20 +46,4 @@ class MegaGreeter
       puts "goodbye #{@names}. Come back soon!"
     end
   end
-end
-
-def time_method(method, *args)
-  beginning_time = Time.now
-  self.send(method, args[0])
-  end_time = Time.now
-  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
-end
-
-learningPath = '~\Ruby\learning\\'
-require learningPath + 'Euler1.rb'
-require learningPath + 'Euler2.rb'
-require learningPath + 'Euler3.rb'
-require learningPath + 'Euler4.rb'
-if __FILE__ == $0
-  time_method(:Problem3, 600851475143)
 end
