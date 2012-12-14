eccdcc1ccbf6a297240d0908ade87f3da1db742b
steve.a.shogren@gmail.com <steve.a.shogren@gmail.com>
2012-12-14 15:43:37 -0500

    redoing the runner and starting 18

--- runner.rb
+++ runner.rb
@@ -1,56 +1,21 @@
-def time_method(method, *args)
+currentProblem = ARGV[0].to_i
+fileName = "~\\Ruby\\learning\\Euler#{currentProblem}.rb"
+require fileName
+
+def time_method(method)
   beginning_time = Time.now
-  ret = self.send(method, args[0])
+  ret = self.send(method)
   end_time = Time.now
   puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   return ret
 end
 
-learningPath = '~\Ruby\learning\\'
-currentProblem = 15
-for i in 1...currentProblem+1 do
-  require "~\\Ruby\\learning\\Euler#{i}.rb"
-end
-
 if __FILE__ == $0
-  res  = time_method("problem#{currentProblem}", 2)
-  if res == 6
+  res  = time_method("test#{currentProblem}")
+  if res == true 
     puts "Test Passes! #{res}"
   else
     puts "Test Failed #{res}"
   end
 end
 
-
-class MegaGreeter
-  attr_accessor :names
-
-  #constructor
-  def initialize(names = "world")
-    @names = names
-  end
-
-  def say_hi
-    if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("each")
-      # @names is a list, iterate
-      @names.each do |name|
-        puts "hello #{name}!"
-      end
-    else
-      puts "hellp #{@names}!"
-    end
-  end
-
-  def say_bye
-    if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("join")
-      # join te list elements with commas
-      puts "goodbye #{@names.join(", ")}. Come back soon!"
-    else
-      puts "goodbye #{@names}. Come back soon!"
-    end
-  end
-end
